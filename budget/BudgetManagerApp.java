8ff47168eb698e28f6237f5121f4e9afca2cfa65
Sirio <sirioroberto1@hotmail.com>
2023-08-14 22:57:24 -0300

    Implemented Analyze command to sort purchases with 3 options

--- budget/BudgetManagerApp.java
+++ budget/BudgetManagerApp.java
@@ -3,9 +3,7 @@ package budget;
 import com.google.gson.Gson;
 
 import java.io.*;
-import java.util.LinkedHashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class BudgetManagerApp {
@@ -40,22 +38,25 @@ public class BudgetManagerApp {
             case "4" -> new ShowBalanceCommand();
             case "5" -> new SaveCommand();
             case "6" -> new LoadCommand();
+            case "7" -> new AnalyzeCommand();
             default -> new ExitCommand();
         };
     }
 
     private void showMenu() {
         System.out.println("""
+                Choose your action:
                 1) Add income
                 2) Add purchase
                 3) Show list of purchases
                 4) Balance
                 5) Save
                 6) Load
+                7) Analyze (Sort)
                 0) Exit""");
     }
 
-    private double getTotal(Set<Purchase> purchases) {
+    private double getTotal(Collection<Purchase> purchases) {
         return purchases.stream()
                 .map(Purchase::getPrice)
                 .reduce(Double::sum)
@@ -110,7 +111,6 @@ public class BudgetManagerApp {
 
                 System.out.println();
             } while (true);
-
         }
     }
 
@@ -217,6 +217,109 @@ public class BudgetManagerApp {
                 throw new RuntimeException(e);
             }
             System.out.println("Purchases were loaded!");
+        }
+    }
+
+    private class AnalyzeCommand extends Command {
+
+        @Override
+        void execute() {
+            String userOption;
+            do {
+                System.out.println("""
+                        How do you want to sort?
+                        1) Sort all purchases
+                        2) Sort by type
+                        3) Sort certain type
+                        4) Back""");
+                userOption = scan.nextLine();
+
+                if ("4".equals(userOption)) {
+                    break;
+                }
+
+                System.out.println();
+                switch (userOption) {
+                    case "1" -> sortAll();
+                    case "2" -> sortByType();
+                    case "3" -> sortCertainType();
+                }
+
+                System.out.println();
+            } while (true);
+        }
+
+        private void sortCertainType() {
+            System.out.println("""
+                    Choose the type of purchase
+                    1) Food
+                    2) Clothes
+                    3) Entertainment
+                    4) Other""");
+            String userOption = scan.nextLine();
+            System.out.println();
+
+            Purchase.Category category = getCategoryFromOption(userOption);
+            List<Purchase> catPurchases = purchases.stream()
+                            .filter(p -> p.getCategory() == category)
+                            .sorted((p1, p2) -> Double.compare(p2.getPrice(), p1.getPrice()))
+                            .toList();
+
+            if (catPurchases.isEmpty()) {
+                System.out.println("The purchase list is empty!");
+            } else {
+                System.out.println(category + ":");
+                catPurchases.forEach(System.out::println);
+                System.out.printf("Total sum: $%.2f\n", getTotal(catPurchases));
+            }
+        }
+
+        private void sortByType() {
+            Map<Purchase.Category, Double> catWithTotalMap = new HashMap<>();
+
+            System.out.println("Types:");
+            for (Purchase.Category category : Purchase.Category.values()) {
+                if (category == Purchase.Category.All) {
+                    continue;
+                }
+                double totalPrice = purchases.stream()
+                        .filter(p -> p.getCategory() == category)
+                        .map(Purchase::getPrice)
+                        .reduce(Double::sum)
+                        .orElse(0.0);
+                catWithTotalMap.put(category, totalPrice);
+            }
+
+            catWithTotalMap.keySet().stream()
+                    .sorted((k1, k2) -> Double.compare(catWithTotalMap.get(k2), catWithTotalMap.get(k1)))
+                    .forEach(k -> {
+                        if (catWithTotalMap.get(k) == 0.0) {
+                            System.out.println(k + " - $0");
+                        } else {
+                            System.out.printf("%s - $%.2f\n", k, catWithTotalMap.get(k));
+                        }
+                    });
+
+            double totalPrice = getTotal(purchases);
+            if (totalPrice == 0.0) {
+                System.out.println("Total sum: $0");
+            } else {
+                System.out.printf("Total sum: $%.2f\n", totalPrice);
+            }
+        }
+
+        private void sortAll() {
+            if (purchases.isEmpty()) {
+                System.out.println("The purchase list is empty!");
+            } else {
+                System.out.println("All:");
+                purchases.stream()
+                        .sorted(Comparator.comparing(Purchase::getCategory))
+                        .sorted((p1, p2) -> Double.compare(p2.getPrice(), p1.getPrice()))
+                        .forEach(System.out::println);
+
+                System.out.printf("Total: $%.2f\n", getTotal(purchases));
+            }
         }
     }
 
